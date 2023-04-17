ae65f2c597dfec8be4b4c317429dfa7ad9ef1f91
Balint Harmat <balint.harmat@betssongroup.com>
2023-04-17 15:34:40 +0200

    PoC
    Note: create local nuget source, and pack/push the InitInNetstandard2.1.0.0.nupkg to it,
    and add the same source to the NetFrameworkConsumingJankyNetstandard2 project.

---
+++ InitInNetstandard2/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace InitInNetstandard2
+{
+    // Note init (C#9) also nullability annotation on reference type. The required (C#11) keyword is not really workable this way.
+    public record Record
+    {
+        public string? Data { get; init; }
+        public IEnumerable<int>? RandomUntypedIds { get; init; }
+    }
+}
---
+++ NetFrameworkConsumingJankyNetstandard2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using InitInNetstandard2;
+
+namespace NetFrameworkConsumingJankyNetstandard2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Record testRecord = new Record
+            {
+                Data = "Hello from 2015, its time to update your runtime",
+                RandomUntypedIds = new []{1,2}
+            };
+            
+            Console.WriteLine(testRecord.Data);
+        }
+    }
+}
